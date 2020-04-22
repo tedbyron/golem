1315f34fe4919541a0f2721c338724cf4b4ab506
Teddy Byron <ted@tedbyron.com>
2020-04-22 18:35:31 -0400

    Add html.jsx

---
+++ src/html.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HTML = ({
+  htmlAttributes,
+  headComponents,
+  bodyAttributes,
+  preBodyComponents,
+  body,
+  postBodyComponents,
+}) => (
+  <html {...htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
+      />
+      {headComponents}
+    </head>
+    <body {...bodyAttributes}>
+      {preBodyComponents}
+      <div
+        key="body"
+        id="___gatsby"
+        dangerouslySetInnerHTML={{ __html: body }}
+      />
+      {postBodyComponents}
+    </body>
+  </html>
+);
+
+export default HTML;
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
