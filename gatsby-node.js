ea67ddb20be78646d78022fb9a7b2bdff2178086
Teddy Byron <ted@tedbyron.com>
2021-07-22 23:53:55 -0400

    update webpack config; add .babelrc

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,12 @@
+const LoadablePlugin = require('@loadable/webpack-plugin');
+
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.wasm$/,
-          type: 'webassembly/sync',
-        },
-      ],
-    },
     experiments: {
       syncWebAssembly: true,
     },
+    plugins: [
+      new LoadablePlugin(),
+    ],
   });
 };
