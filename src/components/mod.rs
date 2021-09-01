cf651f42e77f7a2d261edf7f56fa1891e5e3f971
Teddy Byron <ted@tedbyron.com>
2021-08-31 21:39:31 -0400

    restructure project

---
+++ src/components/mod.rs
@@ -0,0 +1,3 @@
+mod golem;
+
+pub use golem::Golem;
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
-use yew::{html, Component, ComponentLink, Html, ShouldRender};
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+mod components;
 
 fn main() {
-    yew::start_app::<Model>();
-}
-
-#[derive(Debug)]
-pub struct Model;
-impl Component for Model {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        unimplemented!()
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <main>
-                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-                <h1>{ "Hello World!" }</h1>
-                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            </main>
-        }
-    }
+    yew::start_app::<components::Golem>();
 }
